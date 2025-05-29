bf422c1304347a60a05e32024abd1f26cc407bf1
seoywa <132980713+seoywa@users.noreply.github.com>
2025-05-29 11:21:08 +0700

    Update onboarding page: userInfo

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,12 +1,17 @@
-/* @next-codemod-ignore */
+
 
 import AccountProfile from '@/components/forms/AccountProfile'
+import { fetchUser } from '@/lib/actions/user.actions';
 import { currentUser } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
 import React from 'react'
 
 const Page = async () => {
   const user = await currentUser();
-  const userInfo = {};
+  if (!user) return null;
+
+  const userInfo = await fetchUser(user.id);
+  if (userInfo?.onboarded) redirect('/')
   
   const userData = {
     id: user?.id,
